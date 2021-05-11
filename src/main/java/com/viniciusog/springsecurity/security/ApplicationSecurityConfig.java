db5ead313f758c7708da8f3e5d38fd350cb757e9
viniciusog <vinijipa23@gmail.com>
2021-05-11 16:56:34 -0400

    antMatchers

--- src/main/java/com/viniciusog/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/viniciusog/springsecurity/security/ApplicationSecurityConfig.java
@@ -15,14 +15,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //TIPOS DE AUTENTICAÇÃO
 
-        //BASIC AUTH:
+        //BASIC AUTH: (autenticação básica)
         //Requer apenas username e password
         //Requer autenticação todas as vezes que fizermos um request
+        //Quando eu estiver na página index e reiniciar a API, quando eu atualizar a página, o spring
+        //security pede o login novamente
 
-        //Temos aqui a configuração de autenticação básica
-        //Toda vez que fizermos um request, pediremos o usuário e senha novamente
        http
                .authorizeRequests()
+               //Estamos autorizando automaticamente os requests que vierem de algum dos locais abaixo
+               //Ou seja, não precisará de login para acessar as páginas abaixo
+               .antMatchers("/", "index", "/css/*", "/js/*")
+               .permitAll()
+
+               //Autorizando qualquer request, desde que seja autenticado
                .anyRequest()
                .authenticated()
                .and()
